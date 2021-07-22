3a7cd9ffabba01ba01c3abf44ca481fb6c99be1a
Henry Parras <henry.parras@yahoo.com>
2021-07-22 15:04:16 -0700

    made components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home}/>
+        </Switch>
+        <Switch>
+          <Route path="/projects" exact component={Projects}/>
+        </Switch>
+        <Switch>
+          <Route exact path="/contact" component={Contact}/>
+        </Switch>
+        </Router>
+    </>
+  )
 }
-
-export default App;
