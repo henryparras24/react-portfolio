fb54629d3979ee479d93af2e0cb4a1659e29f68f
Henry Parras <henry.parras@yahoo.com>
2021-07-24 18:10:53 -0700

    contact page foundation done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Navbar from "./Navbar";
+import { SocialIcon } from 'react-social-icons';
+
 
 function Contact () {
     return (
             <div>
-                this is contact
+               
+               <h4>Please reach out! I would love to hear from you.</h4>
+                <SocialIcon url="https://www.linkedin.com/in/henry-parras-a98368209/" />
+                <SocialIcon url="https://github.com/henryparras24" />
+                <SocialIcon url="mailto: henry.parras@yahoo.com" />
             </div>
         )
     
