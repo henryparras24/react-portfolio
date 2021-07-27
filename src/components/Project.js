8f4b5cc3050be11d6c86f0f1f0de911e3ff2b004
Henry Parras <henry.parras@yahoo.com>
2021-07-26 17:48:45 -0700

    card for project

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,17 +1,23 @@
 import React from 'react';
 
-function Project({projectsData}){
+function Project({ projectsData }) {
 
-const {name, picture, description, deployedLink , githubLink} = projectsData;
-return (
-        <div>
-            <h6>{name}</h6>
-            <h6>{description}</h6>
-            <h6>{picture}</h6>
-            <h6>{deployedLink}</h6>
-            <h6>{githubLink}</h6>
+    const { name, picture, description, deployedLink, githubLink } = projectsData;
+    return (
+
+
+        <div class="card" style={{width: 18+'rem'}}>
+            <img src={picture} class="card-img-top" alt="..." />
+            <div class="card-body">
+                <h5 class="card-title">{name}</h5>
+                <p class="card-text">{description}</p>
+                <a href={githubLink} class="btn btn-primary">Github</a>
+                <a href={deployedLink} class="btn btn-primary">Deployed</a>
+            </div>
         </div>
-)
+
+
+    )
 }
 
 
